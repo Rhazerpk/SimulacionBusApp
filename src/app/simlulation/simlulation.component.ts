fe9aa93ba09131a5f61978c01f2b4ef098ac6a2d
Rhazerpk <bolshoi19booze@gmail.com>
2024-05-25 16:54:32 -0400

    adding graph

--- src/app/simlulation/simlulation.component.ts
+++ src/app/simlulation/simlulation.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { BusStop, Bus, SimulationStats } from '../interfaces/simulation.model';
 import { SimulationService } from '../services/simulation.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-simulation',
@@ -11,13 +12,52 @@ import { SimulationService } from '../services/simulation.service';
   imports: [CommonModule]
 })
 export class SimulationComponent implements OnInit {
+  @ViewChild('chart') chartRef!: ElementRef<HTMLCanvasElement>;
+  private chart!: Chart;
+
   busStops: BusStop[] = [];
   bus: Bus = new Bus(1, 50);
   stats: SimulationStats = new SimulationStats();
   simulationRunning: boolean = false;
   busStopIds: number[] = [];
 
-  constructor(private simulationService: SimulationService) {}
+  constructor(
+    private simulationService: SimulationService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      import('chart.js/auto').then((module) => {
+        this.chart = new module.Chart(this.chartRef.nativeElement, {
+          type: 'line',
+          data: {
+            labels: this.busStopIds,
+            datasets: [
+              {
+                label: 'Personas Transportadas',
+                data: this.busStopIds.map(id => this.stats.transportadosPorParada[id] || 0),
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+              }
+            ]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            },
+            plugins: {
+              title: {
+                display: true,
+                text: 'Simulación de Autobús'
+              }
+            }
+          }
+        });
+      });
+    }
+  }
 
   ngOnInit() {
     this.busStops = this.simulationService.getBusStops();
@@ -33,6 +73,8 @@ export class SimulationComponent implements OnInit {
   stopSimulation() {
     this.simulationRunning = false;
     this.simulationService.stopSimulation();
+    this.chart.data.datasets[0].data = this.busStopIds.map(id => this.stats.transportadosPorParada[id] || 0);
+    this.chart.update();
   }
 
   simulateArrival(stopId: number) {
